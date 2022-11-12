0867de523a398c30956faf3a55fcea3892f9173e
actuallyzefe <zefek10@gmail.com>
2022-11-12 13:52:30 +0300

    JWT

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,12 +1,23 @@
+const jwt = require('jsonwebtoken'); // npm i jsonwebtoken
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 // async fonskıyonları normalde try catch blockuna yazarız normadle ama daha önceden yarattıgımız catchAsync ile errorlerı yazmakla ugrasmaadan catchliyorus
 
+// exports.signup = catchAsync(async (req, res, next) => {
+// Data işlemi olacağından tabii ki async fonkısoyn kullanacagız
+// const newUser = await User.create(req.body); // buranın hepsi bir promise dondurecegınden onları await ile aktif ediyorz.// create() fonksıyonu ile yeni bir öğe oluştuuryorduk
+// bu yeni öğenin bilgiside request in içindeki body elemanında bulunuyor
+// bunun aynısnı Tour.create() şeklinde yeni tur oluştururken yapmıştık
+
+// IMPORTANT // DIKKAT
+// yukarıda yaptımız tamamıyla dogurydu ama çok buuyk bır guvenlık acıgı vardı. onun yerine bunu kullanacagız:
 exports.signup = catchAsync(async (req, res, next) => {
-  // Data işlemi olacağından tabii ki async fonkısoyn kullanacagız
-  const newUser = await User.create(req.body); // buranın hepsi bir promise dondurecegınden onları await ile aktif ediyorz.// create() fonksıyonu ile yeni bir öğe oluştuuryorduk
-  // bu yeni öğenin bilgiside request in içindeki body elemanında bulunuyor
-  // bunun aynısnı Tour.create() şeklinde yeni tur oluştururken yapmıştık
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
   res.status(201).json({
     status: 'success',
