7ceaa8ca0726cf58872bf0be3ebdfcc56e40c807
actuallyzefe <zefek10@gmail.com>
2022-11-18 22:13:53 +0300

    fix

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -6,11 +6,11 @@ const router = express.Router();
 
 // Users
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.route('/signup').post(authController.signup);
+router.route('/login').post(authController.login);
 
-router.post('/forgotPassword', authController.forgotPassword);
-router.post('/login', authController.login);
+router.route('/forgotPassword').post(authController.forgotPassword);
+router.post('/resetPassword', authController.resetPassword);
 
 router
   .route('/')
