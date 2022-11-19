f17242b872b65ea3b86c231d34d463a22d42a146
actuallyzefe <zefek10@gmail.com>
2022-11-19 20:23:36 +0300

    resetPass

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -219,7 +219,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
   user.passwordResetExpires = undefined;
   await user.save();
 
-  // 3) Update changedPasswordAt property for the user
+  // 3) Update changedPasswordAt property for the user => userModel middleware
   // 4) Log the user in, send JWT
   const token = signToken(user._id);
   res.status(200).json({
