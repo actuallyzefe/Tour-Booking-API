4f98f0998c4b999471adc1898937f3a1efdec35a
actuallyzefe <zefek10@gmail.com>
2022-11-19 18:52:52 +0300

    email

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -10,7 +10,7 @@ router.route('/signup').post(authController.signup);
 router.route('/login').post(authController.login);
 
 router.route('/forgotPassword').post(authController.forgotPassword);
-router.post('/resetPassword', authController.resetPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
 
 router
   .route('/')
--- utils/email.js
+++ utils/email.js
@@ -3,10 +3,12 @@ const nodemailer = require('nodemailer');
 // GENELDE GMAIL VESAIRE YERINE BASKA MAIL UYGULAMALARI ILE MAIL GONDERMEK DAHA MANITKLI
 
 // nodemailer ile email göndermek için 3 aşama takip etmemız gerekiyor
-const sendEmial = (options) => {
+const sendEmail = async (options) => {
   // 1) CREATE A TRANSPORTER => createTransport() içerisine birkaç option alır => kullanılacak service - authanticaton etc
   const transporter = nodemailer.createTransport({
-    service: 'Gmail',
+    // service: 'Gmail',
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_HOST,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
@@ -14,5 +16,14 @@ const sendEmial = (options) => {
     // ACTIVATE IN gmail "LESS SECURE app" option
   });
   // 2) DEFINE THE EMAIL OPTIONS
+  const mailOptions = {
+    from: 'Efe Karakanlı <zefek10@gmail.com>',
+    to: options.email,
+    subject: options.email,
+    text: options.message,
+  };
   // 3) ACTUALLY SEND EMAIL
+  await transporter.sendMail(mailOptions); // this will return a promise so we await it
 };
+
+module.exports = sendEmail;
