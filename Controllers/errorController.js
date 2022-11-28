cdeb9b96735e042be367148bb7e2c4217fec3762
actuallyzefe <zefek10@gmail.com>
2022-11-28 23:43:28 +0300

    errors

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -3,7 +3,7 @@ const appError = require('../utils/appError');
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
-  return new AppError(message, 400);
+  return new appError(message, 400);
 };
 
 const handleDuplicateFieldsDB = (err) => {
@@ -11,13 +11,13 @@ const handleDuplicateFieldsDB = (err) => {
   console.log(value);
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
-  return new AppError(message, 400);
+  return new appError(message, 400);
 };
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
 
   const message = `Invalid input data. ${errors.join('. ')}`;
-  return new AppError(message, 400);
+  return new appError(message, 400);
 };
 
 const handleJWTError = () =>
