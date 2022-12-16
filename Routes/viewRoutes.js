8a1ce305defffc1a4a45438219e38ec94a2f6858
actuallyzefe <zefek10@gmail.com>
2022-12-16 10:39:36 +0300

    render

--- Routes/viewRoutes.js
+++ Routes/viewRoutes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const viewController = require('./../Controllers/viewController');
 
 router.get('/overview', viewController.getOverview);
-router.get('/tour', viewController.getTour);
+router.get('/tour/:slug', viewController.getTour);
 module.exports = router;
