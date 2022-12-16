055ef62beebf918f2c2c9e6e8853e250a15a3594
actuallyzefe <zefek10@gmail.com>
2022-12-16 08:51:03 +0300

    server output vha ged

--- app.js
+++ app.js
@@ -246,6 +246,13 @@ app.use((req, res, next) => {
 // const tourRouter = express.Router();
 // const userRouter = express.Router();
 // mounting rout
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
