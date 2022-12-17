5a79957422c587a71a6903310f11bc634d751381
actuallyzefe <zefek10@gmail.com>
2022-12-17 21:41:21 +0300

    yusuf

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -132,8 +132,8 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.logout = (req, res) => {
-  res.cookie('jwt', 'null', {
-    expires: new Date(Date.now() - 10 * 1000),
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
   });
   res.status(200).json({ status: 'success' });
